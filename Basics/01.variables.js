ee22b24081e4ac6bf4d9ebc8fc07247bef263c03
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2024-03-18 07:44:18 +0000

    variables

--- Basics/01.variables.js
+++ Basics/01.variables.js
@@ -1,1 +1,23 @@
 console.log("hi there!")
+const accountId = 1234
+let accountPassword = 1234
+var accountEmail = "sid@gmail.com"
+accountCity = "Bankura" // In js we can declare variables without using variable keywords
+
+// accountId = 2 // not allowed since if we declare it once then we cant change it further means it will be constant variable.
+
+
+accounEmail="sid@gmail.com"
+accountPassword = 5678
+accountCity = "Jamshedpur" 
+let accountState ; // if we declare a variable but if we dont pass any value onto it then it will execute without error but it will show undefines 
+// In js we use ; or not it will execute
+
+
+// { } this curly braces is called scope.
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// prefer not to use var because of the issue in block scope and functional scope.
+
