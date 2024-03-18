96d4a50a95ca3fcfb908d467c9316d43f53f0998
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2024-03-18 08:25:54 +0000

    Data types

---
+++ Basics/02.Datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treats all js code as newer version
+ let name ="suddeshna"
+ let age = 25
+ let isloggedIn = false
+ let state;
+
+ //datatypes
+  // number = 2 to power 53 an int or floating number
+  //bigint = large number
+  //string = " " used for characters
+ // boolean =  returns true/false value
+ // null = standalone value . it is a typeof object. it is differnt from Null or NULL since js is case-sensitive
+ // undefined = a property or variable whice does not have any value
+ // symbol 
+ // object
+ 
+ console.log(typeof 3)
+ console.log(typeof "sid")
+ console.log(typeof age)
+ console.log(typeof null)
+
