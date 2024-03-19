c120ca6de16fcd72803fa76c3f7bf8da96a8c004
Sudeshna556 <91749997+Sudeshna556@users.noreply.github.com>
2024-03-19 04:45:03 +0000

    datatypes

--- Basics/02.Datatypes.js
+++ Basics/02.Datatypes.js
@@ -19,3 +19,62 @@
  console.log(typeof age)
  console.log(typeof null)
 
+/*
+Based on how data store in Memory datatypes are two types in JS
+1. Primitive Datatypes (Call By Value):
+7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+2. References Type/Non-primitive Datatype:
+Arrays, Objects, Functions
+
+JavaScript is a Dynamically Typed Language
+*/
+
+
+//Symbol Example
+const num = Symbol("123");
+console.log(typeof num);
+
+//BigInt
+const bigNumber = 5465246257627626778789n;
+console.log(typeof bigNumber);
+
+//Arrays:
+const heros = ["Sayan", "Ananya"];
+console.log(typeof heros);
+
+//Objects:
+let myObj = {
+  name: "Sayan",
+  age: 21,
+};
+console.log(typeof myObj);
+
+//Functions:
+const name1 = function () {
+  console.log("Hello World");
+};
+console.log(typeof name1);
+
+//Output of all Datatypes:
+/*
+null: object
+Undefined: undefined
+Symbol: symbol
+BigInt: bigint
+Arrays: object
+Object: object
+Functions: function
+*/
+
+//++++++++++++++++++++++++++++++++++++++
+/*  
+## All Primitive Datatypes are store into Stack
+  # In Stack only copy of a value is pass, 
+  Original one is remain same
+
+## All Non-Primitive Datatypes are store into Heap
+  # In Heap references of value is passed, 
+  if it changed original one is also changed
+
+*/
